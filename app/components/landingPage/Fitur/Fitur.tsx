8378717aa058e272eae153764e5cb48febad2e91
sherinven <sherin.florentina@gmail.com>
2026-07-29 14:13:48 +0700

    benerin Fitur

--- app/components/landingPage/Fitur/Fitur.tsx
+++ app/components/landingPage/Fitur/Fitur.tsx
@@ -1,12 +1,33 @@
 import "./Fitur.css";
 
+const features = [
+  { title: "Analisis Konsumsi" },
+  { title: "Identifikasi Perangkat Boros" },
+  { title: "Insight & Rekomendasi AI" },
+  { title: "Pantau Dampak Anda" },
+];
+
 function Fitur() {
-    return (
-        <div className="Fitur-section">
-            <h2>Kenali Pola Penggunaan Energi Anda</h2>
-            <p>Pahami bagaimana rumah anda menggunakan energi. Kami mengubah data menjadi insight yang membantu Anda mengambil langkah nyata</p>
-        </div>
-    );
+  return (
+    <section className="Fitur-section">
+      <h2>Kenali Pola Penggunaan Energi Anda</h2>
+
+      <p className="fitur-text">
+        Pahami bagaimana rumah Anda menggunakan energi. Kami mengubah data
+        menjadi insight yang membantu Anda mengambil langkah nyata.
+      </p>
+
+      <div className="fitur-container">
+        {features.map((item, index) => (
+          <div className="fitur-card" key={index}>
+            <div className="fitur-box"></div>
+
+            <p >{item.title}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Fitur;
