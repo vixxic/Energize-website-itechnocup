b2741bed43f6e2130db6ffc4fffbcf639c2c3460
vixxic <janikanilobak@gmail.com>
2026-07-29 13:53:10 +0700

    add gsap

--- app/components/landingPage/Problem/Problem.tsx
+++ app/components/landingPage/Problem/Problem.tsx
@@ -1,13 +1,55 @@
 import "./Problem.css";
 
+import { FaArrowTrendUp } from "react-icons/fa6";
+import { BsLightningChargeFill } from "react-icons/bs";
+import { FaLeaf } from "react-icons/fa";
+
+const problemsData = [
+  {
+    title: "Biaya Meningkat",
+    icon: <FaArrowTrendUp />,
+    text: "Semakin boros listik, semakin tinggi tagihan yang harus di bayar",
+    img: "/problems-img/image1.png",
+  },
+  {
+    title: "Energi terbuang",
+    icon: <BsLightningChargeFill />,
+    text: "Semakin boros listik, semakin tinggi tagihan yang harus di bayar",
+    img: "/problems-img/image2.png",
+  },
+  {
+    title: "Dampak Lingkungan",
+    icon: <FaLeaf />,
+    text: "Semakin boros listik, semakin tinggi tagihan yang harus di bayar",
+    img: "/problems-img/image3.png",
+  },
+];
+
 function Problem() {
   return (
     <div className="problem-section padding">
+      <img className="awan-parallax" src="/problems-img/awan.png" />
+
       <h2>Dampak Tersembunyi Penggunaan Energi</h2>
       <p>
         Tagihan listrik bukan satu-satunya yang meningkat ketika kita
         menggunakan energi secara berlebihan
       </p>
+
+      <div className="problems-con">
+        {problemsData.map((problem, index) => (
+          <div key={index}>
+            <img src={problem.img} />
+
+            <div>
+              <div>{problem.icon}</div>
+              <p>{problem.title}</p>
+            </div>
+
+            <p>{problem.text}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
+import { useEffect } from "react";
+
 import Navbar from "./components/Navbar/Navbar";
 
 // landing page
@@ -6,6 +14,46 @@ import Problem from "./components/landingPage/Problem/Problem";
 import Problem2 from "./components/landingPage/Problem2/Problem2";
 
 export default function Home() {
+  useEffect(() => {
+    gsap.fromTo(
+      ".awan-parallax",
+      {
+        y: 0,
+      },
+      {
+        y: "-40%",
+        ease: "none",
+
+        scrollTrigger: {
+          trigger: ".problem-section",
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+          markers: true,
+        },
+      },
+    );
+
+    gsap.fromTo(
+      ".rumah-img",
+      {
+        y: 0,
+      },
+      {
+        y: "40%",
+        ease: "none",
+
+        scrollTrigger: {
+          trigger: ".problem-section",
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+          markers: true,
+        },
+      },
+    );
+  }, []);
+
   return (
     <div className="landing-page">
       <Navbar />
